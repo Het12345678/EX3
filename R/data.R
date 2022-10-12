1b62b5af9769edb8ebab4e094d86d1d9741c8822
Het Patel <het.patel2@students.mq.edu.au>
2022-10-12 14:49:37 +1100

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' @format A data frame with 100 rows and 3 variables:
+#'  \describe{
+#'      \item{X}
+#'      \item{arrival_time}
+#'      \item{service_time}
+#'      }
+#'
+#' @examples
+#' data(bank)
+#' "bank"
+
+?bank
